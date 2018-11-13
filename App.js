2a275eb8a8fc2decfe828d0f101d63b577013707
Gabriel Siqueira <gabreezus@gmail.com>
2018-11-13 11:07:49 -0200

    Login

--- App.js
+++ App.js
@@ -1,62 +1,62 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
-import AppNavigator from './navigation/AppNavigator';
+import axios from 'axios';
+import { StyleSheet, Text, View, TextInput, AlertIOS, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false,
-  };
 
-  render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
-        </View>
-      );
+  constructor(props) {
+    super(props);
+    this.state = { 
+      username: '',
+      senha: '',
+      dados: [],
+      error : false
+    };
+  }
+
+  onPress = () => {
+    if(this.state.username != "" && this.state.senha != "") {
+      const dados = {
+        login: this.state.username,
+        senha: this.state.senha,
+      };
+
+      axios({ 
+          method: 'post', 
+          url: 'http://www.gileduardo.com.br/react/api_charadas/rest.php/auth',
+          headers:{
+            "Content-Type": "application/json" 
+          }, 
+          data: dados
+        }).then(response => {
+          console.log(response.data.nome);
+          console.log(response.data.id);
+        }).catch(error => {
+          alert('Houve um erro inesperado!!!');
+        });
     }
   }
 
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
-    ]);
-  };
-
-  _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
-    console.warn(error);
-  };
-
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
-  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Username:</Text>
+        <TextInput style={{height: 40, width: 150, borderColor: 'gray', borderWidth: 1}} onChangeText={(username) => this.setState({username})} />
+        <Text>Senha:</Text>
+        <TextInput style={{height: 40, width: 150, borderColor: 'gray', borderWidth: 1}} onChangeText={(senha) => this.setState({senha})} />
+        <TouchableOpacity onPress={this.onPress}>
+          <Text>Logar</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
